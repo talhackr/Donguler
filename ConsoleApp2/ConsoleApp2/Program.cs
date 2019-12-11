0759b023afe48ebdd1e0cc5d449fe5de647ebcce
talhackr <58722010+talhackr@users.noreply.github.com>
2019-12-11 21:12:30 +0300

    donguler odev

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //string cumle;
+            //Console.Write("Bir cümle giriniz: ");
+            //cumle = Console.ReadLine();
+            //for (int i = 0; i < cumle.Length; i++)
+            //{
+            //    for (int a = 0; a <= i; a++)
+            //    {
+            //        Console.Write(cumle[a]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+            //-----------------------------------------------------------------
+
+            //string yildiz = "*";
+            //Console.Write("Yükseklik giriniz :");
+            //int yukseklik = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < yukseklik; i++)
+            //{
+            //    Console.WriteLine(yildiz);
+            //    yildiz += "*";
+            //}
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------
+
+            //int sayi, sayac = 0, toplam = 0;
+            //Console.Write("Bir sayı giriniz");
+            //sayi = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    if (i % 7 == 0) //((i % 7 != 0)) 
+            //    {
+            //        sayac++; toplam += i;
+            //        Console.Write("{0} ", i);
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam adeti: {0}", sayac);
+            //Console.WriteLine("Toplamları {0}", toplam);
+            //Console.ReadKey();
+
+            //------------------------------------------------------------------
+
+
+
+            //int sayi;
+            //Console.Write("Bölen sayıyı giriniz...:");
+            //sayi = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % sayi == 0)
+            //    {
+            //        Console.WriteLine("{0} sayısı kalansız bölünür", i);
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+            //------------------------------------------------------------------
+
+            //int toplam = 0;
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    toplam += i;
+            //}
+            //Console.WriteLine("Toplam = {0}", toplam);
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------
+
+            //int toplam = 0, a, b;
+
+            //Console.WriteLine("bir sayi girin");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("bir sayi girin");
+            //b = int.Parse(Console.ReadLine());
+            //for (int i = a; i <= b; i++)
+            //{
+            //    toplam += i;
+            //}
+            //Console.WriteLine("Toplam : {0}", toplam);
+            //Console.ReadKey();
+
+            //--------------------------------------------------------------------------------------
+
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("CİHAN HOCA THE BEST\n");
+            //}
+
+            //Console.ReadKey();
+
+
+            //-------------------------------------------------------------------------------
+
+
+            //Console.Write("Metni Gir : ");
+            //string metin = Console.ReadLine();
+            //Console.WriteLine("Kaç tekrar olsun");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    Console.WriteLine(metin);
+            //}
+            //Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------+
+
+            //Console.WriteLine("bir boyut giriniz");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    for (int j = i; j <= a; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+
+
+
+        }
+    }
+}
